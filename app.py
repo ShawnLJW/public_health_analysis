419636bf576e8ce9f8512d870534de2ca8af6b97
Shawn Lee Jia Wei <thisisshawnlee@gmail.com>
2023-02-19 15:42:16 +0800

    adding basic layouts

--- app.py
+++ app.py
@@ -7,32 +7,54 @@ app = Dash('health_dashboard')
 def index():
     layout = html.Div(children=[
         html.H1(children='How Healthy are Singaporeans?'),
-
-        dcc.Graph(
-            id='life_expectancy',
-            figure=plot_life_expectancy()
-        ),
+        
+        html.Div(children=[
+            dcc.Graph(
+                id='life_expectancy',
+                figure=plot_life_expectancy()
+            ),
+            
+            dcc.Graph(
+                id='obesity_rate',
+                figure=plot_obesity_rate()
+            ),
+        ], className='dual-grid'),
 
         dcc.Graph(
             id='aging_population',
             figure=plot_ageing_population()
         ),
+        
+        html.Div(children=[
+            dcc.Graph(
+                id='conditions',
+                figure=plot_conditions()
+            ),
+            
+            dcc.Markdown('''
+                # This is an <h1> tag
 
-        dcc.Graph(
-            id='obesity_rate',
-            figure=plot_obesity_rate()
-        ),
+                ## This is an <h2> tag
 
-        dcc.Graph(
-            id='conditions',
-            figure=plot_conditions()
-        ),
+                ###### This is an <h6> tag
+            ''')
+        ], className='dual-grid'),
+        
+        html.Div(children=[
+            dcc.Graph(
+                id='causes_of_death',
+                figure=plot_deaths()
+            ),
+            
+            dcc.Markdown('''
+                # This is an <h1> tag
 
-        dcc.Graph(
-            id='causes_of_death',
-            figure=plot_deaths()
-        )
-    ], className='dashboard-contents')
+                ## This is an <h2> tag
+
+                ###### This is an <h6> tag
+            ''')
+        ], className='dual-grid')
+    ], id='dashboard-contents')
     return layout
 
 
